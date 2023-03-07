4ef2661ed8dd5cfe44d86d341c23ce81ac1a782f
Arthur Lino <arthur.ssLino@gmail.com>
2023-03-07 12:52:05 -0300

    🐍 feat: day 26 - KeyError Handling

--- day026 - NATO Alphabet/main.py
+++ day026 - NATO Alphabet/main.py
@@ -1,10 +1,18 @@
 import pandas
 
+
+def get_phonetic_code():
+    try:
+        user_word = input("Enter a word to get it's phonetic code: ").upper()
+        word_codes = [codes_dict[letter] for letter in user_word]
+        for code in word_codes:
+            print(f"{code[0]}: {code}")
+    except KeyError:
+        print("Only letters of the alphabet are valid. Try again.")
+        get_phonetic_code()
+
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 codes_dict = {row.letter: row.code for index, row in data.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Enter a word to get the it's phonetic code: ").upper()
-word_codes = [codes_dict[letter] for letter in user_word]
-for code in word_codes:
-    print(f"{code[0]}: {code}")
+get_phonetic_code()
